21fd938e75f96e60d29e35d86a0e42ffaa0ca2f4
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2023-04-02 17:13:20 +0100

    buy button that doesn't buy yet

--- src/state/game/hud.rs
+++ src/state/game/hud.rs
@@ -9,28 +9,57 @@ pub(super) struct Hud;
 
 impl Plugin for Hud {
     fn build(&self, app: &mut App) {
-        app.add_system(display_money.in_schedule(OnEnter(AppState::Game)))
+        app.add_system(display_hud.in_schedule(OnEnter(AppState::Game)))
             .add_system(update_money.in_set(OnUpdate(AppState::Game)));
     }
 }
 
 #[derive(Component)]
+struct BuyButton;
+
+#[derive(Component)]
 struct MoneyDisplay;
 
-fn display_money(mut commands: Commands, fonts: Res<Fonts>) {
+fn display_hud(mut commands: Commands, fonts: Res<Fonts>) {
     commands
-        .spawn(
-            TextBundle::from_section(
-                "Money:",
-                TextStyle {
-                    font: fonts.default_font.clone(),
-                    font_size: 30.0,
-                    color: Color::BLACK,
-                },
-            )
-            .with_text_alignment(TextAlignment::Left),
-        )
-        .insert(MoneyDisplay);
+        .spawn(NodeBundle {
+            style: Style {
+                flex_direction: FlexDirection::Column,
+                size: Size::width(Val::Percent(10.0)),
+                align_items: AlignItems::FlexStart,
+                justify_content: JustifyContent::FlexStart,
+                margin: UiRect::all(Val::Percent(1.0)),
+                gap: Size::all(Val::Percent(1.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(
+                    TextBundle::from_section(
+                        "Money:",
+                        TextStyle {
+                            font: fonts.default_font.clone(),
+                            font_size: 30.0,
+                            color: Color::BLACK,
+                        },
+                    )
+                    .with_text_alignment(TextAlignment::Left),
+                )
+                .insert(MoneyDisplay);
+
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                        ..default()
+                    },
+                    background_color: BackgroundColor::from(Color::GRAY),
+                    ..default()
+                })
+                .insert(BuyButton);
+        });
 }
 
 fn update_money(
