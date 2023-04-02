48286c62d6b0da8227e01212bf07da505a941b90
sousajo-cc <jorge.pinto.sousa@protonmail.ch>
2023-04-02 22:22:07 +0200

    Remove unsafe block from shoot

--- src/state/game/shooting.rs
+++ src/state/game/shooting.rs
@@ -28,7 +28,7 @@ fn shoot(
     mut towers: Query<(&Transform, &ShootRadius, &mut ShootTimer), With<Tower>>,
     enemies: Query<(&Transform, &Velocity), With<Enemy>>,
 ) {
-    for (tower_transform, tower_shoot_radius, mut tower_shoot_timer) in towers.iter_mut() {
+     for (tower_transform, tower_shoot_radius, mut tower_shoot_timer) in towers.iter_mut() {
         if tower_shoot_timer.tick(time.delta()).finished() {
             let distance_to_tower = |enemy_transform: &&Transform| {
                 (enemy_transform.translation - tower_transform.translation).length()
@@ -40,9 +40,8 @@ fn shoot(
                 })
                 .min_by_key(|(transform, _velocity)| {
                     (distance_to_tower(transform) * 100.0) as u128
-                }); //TODO: remove this horrible hack to compare floats
-            let Some((target_transform, target_velocity)) = closest_enemy else { continue };
-            let velocity = unsafe {
+                });
+            if let Some((target_transform, target_velocity)) = closest_enemy {
                 // some mathy stuff lol TODO: refactor
                 // TODO: consider divisions by zero
                 let x1 = target_transform.translation.x;
@@ -58,25 +57,25 @@ fn shoot(
                 let b = -2.0 * beta;
                 let c = (beta + alpha * v) * (beta - alpha * v);
                 let delta = b * b - 4.0 * a * c;
-                let vx_solution1 = (-b - sqrtf32(delta)) / (2.0 * a);
-                let vx_solution2 = (-b + sqrtf32(delta)) / (2.0 * a);
+                let vx_solution1 = (-b - delta.sqrt()) / (2.0 * a);
+                let vx_solution2 = (-b + delta.sqrt()) / (2.0 * a);
                 let vx = if x1 < x2 { vx_solution1 } else { vx_solution2 };
-                let vy_solution1 = -sqrtf32(v * v - vx * vx);
-                let vy_solution2 = sqrtf32(v * v - vx * vx);
+                let vy_solution1 = -((v * v - vx * vx).sqrt());
+                let vy_solution2 = (v * v - vx * vx).sqrt();
                 let vy = if y1 < y2 { vy_solution1 } else { vy_solution2 };
-                Velocity::new(vx, vy)
-            };
-            tower_shoot_timer.reset();
-            commands
-                .spawn(SpriteBundle {
-                    texture: sprites.bullet.clone(),
-                    transform: Transform::from_translation(tower_transform.translation)
-                        .with_scale(Vec3::splat(0.05)),
-                    ..Default::default()
-                })
-                .insert(velocity)
-                .insert(Damage::new(1.0))
-                .insert(Bullet);
+                let velocity = Velocity::new(vx, vy);
+                tower_shoot_timer.reset();
+                commands
+                    .spawn(SpriteBundle {
+                        texture: sprites.bullet.clone(),
+                        transform: Transform::from_translation(tower_transform.translation)
+                            .with_scale(Vec3::splat(0.05)),
+                        ..Default::default()
+                    })
+                    .insert(velocity)
+                    .insert(Damage::new(1.0))
+                    .insert(Bullet);
+            }
         }
     }
 }
