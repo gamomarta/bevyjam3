5c8f4f82d0bb1a1343bac5ed88a88451d2e03a0b
Luiz Chagas Jardim <luiz.jardim@velocix.com>
2023-04-07 10:38:57 +0100

    enemy wobble

--- src/state/game/wobble.rs
+++ src/state/game/wobble.rs
@@ -1,5 +1,6 @@
 use crate::constants::TOWER_WOBBLE_DURATION;
 use crate::state::game::bullet::Bullet;
+use crate::state::game::enemy::Enemy;
 use crate::state::game::tower::Tower;
 use crate::state::AppState;
 use bevy::prelude::*;
@@ -9,7 +10,8 @@ pub(super) struct Wobble;
 impl Plugin for Wobble {
     fn build(&self, app: &mut App) {
         app.add_system(shoot_wobble.in_set(OnUpdate(AppState::Game)))
-            .add_system(bullet_rotation.in_set(OnUpdate(AppState::Game)));
+            .add_system(bullet_rotation.in_set(OnUpdate(AppState::Game)))
+            .add_system(enemy_walk.in_set(OnUpdate(AppState::Game)));
     }
 }
 
@@ -46,5 +48,12 @@ fn shoot_wobble(
 fn bullet_rotation(mut bullets: Query<&mut Transform, With<Bullet>>, time: Res<Time>) {
     for mut transform in bullets.iter_mut() {
         transform.rotation = Quat::from_axis_angle(Vec3::Z, 3.0 * time.elapsed_seconds());
+    }
+}
+
+fn enemy_walk(mut enemies: Query<&mut Transform, With<Enemy>>, time: Res<Time>) {
+    for mut transform in enemies.iter_mut() {
+        transform.rotation =
+            Quat::from_axis_angle(Vec3::Z, 0.1 * (7.0 * time.elapsed_seconds()).sin());
     }
 }
